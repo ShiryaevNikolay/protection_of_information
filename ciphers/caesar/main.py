29a85d04148bd08b67d96471d39c5a2e06b13c37
ShiryaevNikolay <deendeeps@list.ru>
2021-09-12 11:06:13 +0300

    Добавляет обработку пользщовательского ввода

--- ciphers/caesar/main.py
+++ ciphers/caesar/main.py
@@ -1,7 +1,37 @@
+import os
+import signal
 from cipher_caesar import cipher_caesar_encrypt
 
+signal.signal(signal.SIGINT, lambda *_: os._exit(1))  # Для корректного выхода из программы
+
+answers = [1, 2]
+
+print("Выберите, что нужно сделать с сообщением:")
+print("{encrypt} Зашифровать".format(encrypt=answers[0]))
+print("{decrypt} Расшифровать".format(decrypt=answers[1]))
+while True:
+    try:
+        select = int(input("Ответ: "))
+        if select in answers:
+            break
+        else:
+            print("Введите", end=" ")
+            for index, value in enumerate(answers):
+                if index == len(answers) - 1:
+                    print(value)
+                    break
+                print(value, end=', ')
+    except:
+        print("Неверный ввод")
+
 message = input("Введите сообщение, которое нужно зашифровать: ")
-key = int(input("Введите ключ: "))
+
+while True:
+    try:
+        key = int(input("Введите ключ: "))
+        break
+    except:
+        print("Неверный ввод")
 
 encrypted_message = cipher_caesar_encrypt(message, key)
 
